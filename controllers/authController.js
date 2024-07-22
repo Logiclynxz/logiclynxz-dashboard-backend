d0608e1f0beeac0954b1b2f1e98eae05c8fa3f37
aby calago <calagoaby@gmail.com>
2024-07-22 18:37:48 +0800

    feat: add JWT token (not complete)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/UserModel");
 
 const login = async (req, res) => {
@@ -12,9 +14,26 @@ const login = async (req, res) => {
     if (user) {
       const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
+        const accessToken = jwt.sign(
+          { username: user.username, email: user.email },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "30s" }
+        );
+        const refreshToken = jwt.sign(
+          { username: user.username, email: user.email },
+          process.env.REFRESH_TOKEN_SECRET,
+          { expiresIn: "1d" }
+        );
+
+        const user = await User.findByIdAndUpdate(user._id, { refreshToken });
+
+        res.cookie("jwt", refreshToken, {
+          httpOnly: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        });
         return res
           .status(200)
-          .json({ message: "User authenticated", success: true });
+          .json({ message: "User authenticated", success: true, accessToken, refreshToken });
       } else {
         return res.status(400).json({
           message: "Invalid username/email or password",
@@ -22,12 +41,10 @@ const login = async (req, res) => {
         });
       }
     } else {
-      return res
-        .status(400)
-        .json({
-          message: "Invalid username/email or password",
-          success: false,
-        });
+      return res.status(400).json({
+        message: "Invalid username/email or password",
+        success: false,
+      });
     }
   } catch (error) {
     return res
--- models/UserModel.js
+++ models/UserModel.js
@@ -38,6 +38,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    refreshToken: {
+      type: String, 
+      default: "",
+    },
   },
   { timestamps: true }
 );
